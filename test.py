3506973681fc81400ec98cdf6da580cd3ebe5379
Vilush <viola.faltejskova@gmail.com>
2023-07-28 13:39:18 +0200

    Test

---
+++ test.py
@@ -0,0 +1,69 @@
+# name = input("Jaké je tvé jméno?\n")
+# print("Ahoj, já jsem " + name)
+
+# age = 40
+# age = age + 1
+# print(age)
+
+# city = input("V jakém městě bydlíš?\n")
+# print("Bydlím ve městě " + city)
+
+# print(len("Ahoj"))
+
+# name = input("Zadejte své jméno\n")
+# length = len(name)
+# print(name)
+# print(length)
+
+# length = len(input("Zadejte své jméno\n"))
+# print(length)
+
+# n = "Violka"
+# l = 7
+
+# name = "Violka"
+# length = 7
+
+# name1 = "Violka"
+# print(name1)
+
+# print("Vítej v aplikaci na generování vtipných jmen")
+# name = input("Jaké je tvé křestní jméno?\n")
+# vlastnost = input("Jaká je tvá typická vlastnost? Napiš ji s velkým písmenem\n")
+# print("Tvoje vtipné jméno je " + name + " " + vlastnost)
+
+# age = 19
+# print(type(age))
+# age = str(19)
+# print(type(age))
+# print("Ahoj, já jsem Violka a je mi " + age + " let")
+
+# age = 19
+# print("Ahoj, já jsem Violka a je mi " + str(age) + " let")
+
+# dvoumistne_cislo = input("Napiště dvoumístné číslo\n")
+# prvni_cislo = int(dvoumistne_cislo[0])
+# druhe_cislo = int(dvoumistne_cislo[1])
+# print(prvni_cislo + druhe_cislo)
+
+# vyska = input("Zadejte svou výšku v metrech:\n")
+# vaha = input("Zadejte svou váhu v kg:\n")
+
+# BMI = int(vaha) / float(vyska)**2
+# BMI = int(BMI)
+# print("Váš BMI je:" + str(BMI))
+
+# print(8/3)
+# print(int(8/3))
+# print(8//3)
+# print(round(8/3))
+# print(round(5/2))
+# print(round(8/3.4))
+
+# x = 1
+# x = x + 1    # x + = 1
+# x = x - 1    # x -= 1
+# x = x * 2    # x *= 2
+# x = x / 2    # x /= 2
+# print(x)´
+
